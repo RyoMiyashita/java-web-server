64a2292ed58da5d63b7dce5339660529ef52a4e2
RyoMiyashita <miyazi1230@gmail.com>
2021-07-03 11:04:16 +0900

    Initial commit

---
+++ src/WebServer.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+
+    public static void main(final String [] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket()) {
+            serverSocket.bind(new InetSocketAddress(1230));
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+
+                try (InputStream in = socket.getInputStream(); OutputStream out = socket.getOutputStream()) {
+                    int readByte = in.read();
+                    out.write(readByte);
+
+                    System.out.println("received: " + String.valueOf(readByte));
+                }
+            }
+        }
+    }
+}
