bf34f85d98f5c1dbdde7efd945215347275462a5
RyoMiyashita <miyazi1230@gmail.com>
2021-07-03 12:25:51 +0900

    echo server

--- src/WebServer.java
+++ src/WebServer.java
@@ -1,24 +1,34 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class WebServer {
+    private final static String CLOSE_STRING = "quit";
 
     public static void main(final String [] args) throws IOException {
         try (ServerSocket serverSocket = new ServerSocket()) {
+            serverSocket.setReuseAddress(true);
             serverSocket.bind(new InetSocketAddress(12345));
 
             while (true) {
                 Socket socket = serverSocket.accept();
 
-                try (InputStream in = socket.getInputStream(); OutputStream out = socket.getOutputStream()) {
-                    int readByte = in.read();
-                    out.write(readByte);
+                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
 
-                    System.out.println("received: " + String.valueOf(readByte));
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        System.out.println("received : " + line);
+                        if (CLOSE_STRING.equals(line)) {
+                            break;
+                        }
+
+                        bufferedWriter.write(line + '\n');
+                        bufferedWriter.flush();
+                    }
+                    bufferedWriter.write("bye\n");
+                    bufferedWriter.flush();
                 }
             }
         }
